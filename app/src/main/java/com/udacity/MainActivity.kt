6ffdb597b640cc81fce763463ce66bf679debab9
Tejas <Tejas75O25@gmail.com>
2021-09-09 18:39:29 +0530

    Finished radio buttons working and toast messaging on download failed.

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -8,14 +8,17 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.jar.Manifest
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,7 +36,13 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_loading_button.setOnClickListener {
-            download()
+            val url = when(radio_group.checkedRadioButtonId) {
+                2131230865 -> "https://github.com/bumptech/glide"
+                2131230896 -> "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+                2131230934 -> "https://github.com/square/retrofit"
+                else -> ""
+            }
+            download(url)
         }
     }
 
@@ -46,24 +55,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
-        custom_loading_button.startDownload()
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+    private fun download(URL: String) {
+        if (URL.isNotBlank()) {
+            val request =
+                DownloadManager.Request(Uri.parse(URL))
+                    .setTitle(getString(R.string.app_name))
+                    .setDescription(getString(R.string.app_description))
+                    .setRequiresCharging(false)
+                    .setAllowedOverMetered(true)
+                    .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadID =
+                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            custom_loading_button.startDownload()
+        } else {
+            Toast.makeText(applicationContext, "Please select the file download", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
